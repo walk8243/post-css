62e27ddb15bb5f8139a5e36ae306c3f75c77de67
walk8243 <walk8243@gmail.com>
2018-08-18 20:33:13 +0900

    targetを設定

--- sass.js
+++ sass.js
@@ -1,4 +1,23 @@
 class Sass {
+  constructor(directory, filter, recursion) {
+    this.setTarget(directory, filter, recursion);
+  }
+
+  getTarget() {
+    return {
+      directory: this.directory,
+      filter: this.filter,
+      recursion: this.recursion,
+    };
+  }
+  setTarget(directory = '', filter = '', recursion = false) {
+    if(directory.match(/\/$/)) directory = directory.slice(0, -1);
+    Object.assign(this, {
+      directory: directory,
+      filter: filter,
+      recursion: recursion,
+    });
+  }
 }
 
 module.exports = Sass;
